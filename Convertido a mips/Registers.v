20feedcbc15116a66a49c9735e9ec8ba56f75c1b
Frander Granados <fgranados@ic-itcr.ac.cr>
2017-06-04 16:49:43 -0600

    Se agrega PC register y Registers

---
+++ Convertido a mips/Registers.v
@@ -0,0 +1,72 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Module Name:    Registers 
+// Description - Implements a register file with 32 32-Bit wide registers. 
+//
+// INPUTS:-
+// ReadRegister1: 5-Bit address to select a register to be read through 32-Bit 
+//                output port 'ReadRegister1'.
+// ReadRegister2: 5-Bit address to select a register to be read through 32-Bit 
+//                output port 'ReadRegister2'.
+// WriteRegister: 5-Bit address to select a register to be written through 32-Bit
+//                input port 'WriteRegister'.
+// WriteData: 32-Bit write input port.
+// RegWrite: 1-Bit control input signal.
+//
+// OUTPUTS:-
+// ReadData1: 32-Bit registered output. 
+// ReadData2: 32-Bit registered output. 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Registers(ReadRegister1, ReadRegister2, WriteRegister, WriteData, RegWrite, Clk, ReadData1, ReadData2);
+
+
+	input [4:0] ReadRegister1,ReadRegister2,WriteRegister;
+	input [31:0] WriteData;
+	input RegWrite,Clk;
+	
+	output reg [31:0] ReadData1,ReadData2;
+	
+	reg [31:0] Registers [0:31];
+	
+	initial begin
+		Registers[0] <= 32'h00000000;
+		Registers[8] <= 32'h00000000;
+		Registers[9] <= 32'h00000000;
+		Registers[10] <= 32'h00000000;
+		Registers[11] <= 32'h00000000;
+		Registers[12] <= 32'h00000000;
+		Registers[13] <= 32'h00000000;
+		Registers[14] <= 32'h00000000;
+		Registers[15] <= 32'h00000000;
+		Registers[16] <= 32'h00000000;
+		Registers[17] <= 32'h00000000;
+		Registers[18] <= 32'h00000000;
+		Registers[19] <= 32'h00000000;
+		Registers[20] <= 32'h00000000;
+		Registers[21] <= 32'h00000000;
+		Registers[22] <= 32'h00000000;
+		Registers[23] <= 32'h00000000;
+		Registers[24] <= 32'h00000000;
+		Registers[25] <= 32'h00000000;
+		Registers[29] <= 32'd252; //Parte superior de la memoria,
+		Registers[31] <= 32'b0;
+	end
+	
+	
+	always @(posedge Clk)
+	begin
+		
+		if (RegWrite == 1) 
+		begin
+			Registers[WriteRegister] <= WriteData;
+		end
+	end
+	
+	always @(negedge Clk)
+	begin
+		ReadData1 <= Registers[ReadRegister1];
+		ReadData2 <= Registers[ReadRegister2];
+	end
+	
+endmodule	
