a7d1307e660a87675a630dab82b92e212dadfb49
Frander Granados <fgranados@ic-itcr.ac.cr>
2017-06-06 01:12:54 -0600

    Se agrega extención de signo

--- Convertido a mips/SignExtend.v
+++ Convertido a mips/SignExtend.v
@@ -18,8 +18,31 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module SignExtend(
-    );
 
+module SignExtend(out, in,ExtendSign);
+	 
+	 /* A 32-Bit output word */
+    output  [31:0] out;
+    
+    /* A 16-Bit input word */
+    input   [15:0] in;
+	 input ExtendSign;
+	 reg [31:0] out;
+
+    always@(in,ExtendSign)
+	 begin
+		if (ExtendSign == 0) begin
+			out <= {16'h0000 , in};
+		end else begin
+			if (in[15]==1)
+			begin
+			  out <= {16'hffff , in};
+			end
+			else 
+			begin
+			  out <= in;
+			end
+		end
+	 end
 
 endmodule
