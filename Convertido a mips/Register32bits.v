e59f51f4ce1282c3d9eade7c09f753b864477bbc
Frander Granados <fgranados@ic-itcr.ac.cr>
2017-06-06 01:00:42 -0600

    Se agregan registros faltantes y su respectivo test

---
+++ Convertido a mips/Register32bits.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:01:14 06/04/2017 
+// Design Name: 
+// Module Name:    ShiftLeft26 
+// Project Name: 
+// Target Devices: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Register32bits(in, out, Clk);
+
+	input [31:0]in;
+	input Clk;
+	output [31:0]out;
+	
+	reg [31:0] r_reg;
+
+	always @ (posedge Clk)
+		begin
+			r_reg = in;
+		end
+	assign out = r_reg;
+	
+endmodule 
---
+++ Convertido a mips/test/testRegister32bits.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   00:54:05 06/06/2017
+// Design Name:   Register32bits
+////////////////////////////////////////////////////////////////////////////////
+
+module testRegister32bits;
+
+	// Inputs
+	reg [31:0] in;
+	reg Clk;
+
+	// Outputs
+	wire [31:0] out;
+
+	// Instantiate the Unit Under Test (UUT)
+	Register32bits uut (
+		.in(in), 
+		.out(out), 
+		.Clk(Clk)
+	);
+
+	initial begin
+		// Initialize Inputs
+		in = 0;
+		Clk = 0;
+
+		#10 in = 32'b11011100111111111111111111111111;
+
+	end
+	
+	always
+		begin
+			#10 Clk <= ~Clk;
+		end
+      
+endmodule
+
