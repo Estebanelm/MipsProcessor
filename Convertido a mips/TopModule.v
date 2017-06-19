53ccb0caec004bfdedc07ea889a6a7c66003eb77
Frander Granados <fgranados@ic-itcr.ac.cr>
2017-06-18 23:48:14 -0600

    Se corrigen datos en el topModule

--- Convertido a mips/TopModule.v
+++ Convertido a mips/TopModule.v
@@ -1,26 +1,14 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: Digilent Inc.
-// Engineer: Thomas Kappenman
 // 
-// Create Date: 03/03/2015 09:06:31 PM
+// 
+// Creator: Esteban Calvo, Frander Granados
 // Design Name: 
 // Module Name: top
-// Project Name: Nexys4DDR Keyboard Demo
-// Target Devices: Nexys4DDR
-// Tool Versions: 
-// Description: This project takes keyboard input from the PS2 port,
-//  and outputs the keyboard scan code to the 7 segment display on the board.
-//  The scan code is shifted left 2 characters each time a new code is
-//  read.
+// Project Name: MipsProcessos
 // 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////// 
 
 
 module TopModule(
